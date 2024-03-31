6ce12f03b78835a89803fb656ccf1807995ba620
Farhan <farhankarim5678@gmail.com>
2024-03-31 17:14:33 +0500

    list files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import Homepage from "./routes/homepage/homepage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+
 function App() {
-  return (
-    <div>Hello World</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
